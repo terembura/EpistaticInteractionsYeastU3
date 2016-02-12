f18cdb7788451edc75358889508dfe7e0808ee8e
Tokos Tohotom <totom@wikileaks.gov>
2016-02-12 10:37:17 +0000

    new

--- utils/process_snp_file_extra.m
+++
@@ -1,5 +0,0 @@
-function d = process_snp_file_extra(fname)
-A                  = readtable_string(fname);
-d                  = domatrixunq(A);
-d.filename         = A.fname;   
-d.processing_file  = 'process_snp_file_extra';    
